27724da4c86a41e184752764c0a4bc85b6391e97
90534 <90534@CAN>
2023-06-20 20:34:29 +0300

    20. Gün 2. Mola

--- src/app/components/navi/navi.component.ts
+++ src/app/components/navi/navi.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navi',
   templateUrl: './navi.component.html',
   styleUrls: ['./navi.component.css']
 })
-export class NaviComponent {
-
+export class NaviComponent implements OnInit {
+  constructor(private router: Router){}
+  ngOnInit(): void {
+    
+  }
+  redirectToProductAdd() {
+    console.log('redirectToProductAdd() fonksiyonu çağrıldı');
+    this.router.navigateByUrl('/products/add');
+  }
 }
